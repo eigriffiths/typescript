c39f24521d5bd096351dab0aaa921622df40b3e0
Emyr Griffiths <Emyr.Griffiths@denbighshire.gov.uk>
2023-04-20 18:37:50 +0100

    object arrays

---
+++ sandbox.js
@@ -0,0 +1,18 @@
+// arrays
+var names = ['luigi', 'mario', 'yoshi'];
+names.push('toad');
+names.push(22);
+var numbers = [10, 20, 30, 40];
+numbers.push(25);
+// numbers.push('emyr')
+var mixed = ['ken', 4, 'emyr', 8, 9];
+mixed.push('ryu');
+mixed.push(10);
+// objects
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+ninja.age = 40;
+ninja.name = 'ryu';
---
+++ sandbox.ts
@@ -0,0 +1,25 @@
+// arrays
+let names = ['luigi', 'mario', 'yoshi'];
+
+names.push('toad');
+names.push(22);
+
+let numbers = [10, 20, 30, 40];
+
+numbers.push(25);
+// numbers.push('emyr')
+
+let mixed = ['ken', 4, 'emyr', 8, 9]
+
+mixed.push('ryu')
+mixed.push(10)
+
+// objects
+let ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
+};
+
+ninja.age = 40
+ninja.name = 'ryu'
